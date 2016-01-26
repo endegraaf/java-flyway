bdd30d68e665ea179fca94441e5e4ac5daf25d41
eric <eric@eric-Latitude-E7440>
2016-01-26 13:37:45 +0100

    Initial dataset 10 records V4 in resources/db/migration

---
+++ src/main/resources/db/migration/V4__Insert_some_data.sql
@@ -0,0 +1,55 @@
+/*
+	Initial data set 50 records.
+*/
+INSERT INTO contacts (name, contacts) VALUES ('Liam Bakker', 'Bakker');
+INSERT INTO contacts (name, contacts) VALUES ('Sem Beek', 'Beek');
+INSERT INTO contacts (name, contacts) VALUES ('Lucas Berg', 'Berg');
+INSERT INTO contacts (name, contacts) VALUES ('Luuk Boer', 'Boer');
+INSERT INTO contacts (name, contacts) VALUES ('Noah Bos', 'Bos');
+INSERT INTO contacts (name, contacts) VALUES ('Milan Bosch', 'Bosch');
+INSERT INTO contacts (name, contacts) VALUES ('Daan Brink', 'Brink');
+INSERT INTO contacts (name, contacts) VALUES ('Levi Broek', 'Broek');
+INSERT INTO contacts (name, contacts) VALUES ('Finn Brouwer', 'Brouwer');
+INSERT INTO contacts (name, contacts) VALUES ('Jesse Bruin', 'Bruin');
+/* optional
+INSERT INTO contacts (name, contacts) VALUES ('Max Dam', 'Dam');
+INSERT INTO contacts (name, contacts) VALUES ('Thomas Dekker', 'Dekker');
+INSERT INTO contacts (name, contacts) VALUES ('Bram Dijk', 'Dijk');
+INSERT INTO contacts (name, contacts) VALUES ('Thijs Dijkstra', 'Dijkstra');
+INSERT INTO contacts (name, contacts) VALUES ('Sam Graaf', 'Graaf');
+INSERT INTO contacts (name, contacts) VALUES ('Tim Groot', 'Groot');
+INSERT INTO contacts (name, contacts) VALUES ('Lars Haan', 'Haan');
+INSERT INTO contacts (name, contacts) VALUES ('Ruben Hendriks', 'Hendriks');
+INSERT INTO contacts (name, contacts) VALUES ('Julian Heuvel', 'Heuvel');
+INSERT INTO contacts (name, contacts) VALUES ('Adam Hoek', 'Hoek');
+INSERT INTO contacts (name, contacts) VALUES ('Jayden Jacobs', 'Jacobs');
+INSERT INTO contacts (name, contacts) VALUES ('Mees Jansen', 'Jansen');
+INSERT INTO contacts (name, contacts) VALUES ('Noud Janssen', 'Janssen');
+INSERT INTO contacts (name, contacts) VALUES ('Gijs Jong', 'Jong');
+INSERT INTO contacts (name, contacts) VALUES ('Benjamin Klein', 'Klein');
+INSERT INTO contacts (name, contacts) VALUES ('Emma Kok', 'Kok');
+INSERT INTO contacts (name, contacts) VALUES ('Julia Koning', 'Koning');
+INSERT INTO contacts (name, contacts) VALUES ('Sophie Koster', 'Koster');
+INSERT INTO contacts (name, contacts) VALUES ('Anna Leeuwen', 'Leeuwen');
+INSERT INTO contacts (name, contacts) VALUES ('Mila Linden', 'Linden');
+INSERT INTO contacts (name, contacts) VALUES ('Eva Maas', 'Maas');
+INSERT INTO contacts (name, contacts) VALUES ('Tess Meer', 'Meer');
+INSERT INTO contacts (name, contacts) VALUES ('Lotte Meijer', 'Meijer');
+INSERT INTO contacts (name, contacts) VALUES ('Sara Mulder', 'Mulder');
+INSERT INTO contacts (name, contacts) VALUES ('Zoë Peters', 'Peters');
+INSERT INTO contacts (name, contacts) VALUES ('Evi Ruiter', 'Ruiter');
+INSERT INTO contacts (name, contacts) VALUES ('Fenna Schouten', 'Schouten');
+INSERT INTO contacts (name, contacts) VALUES ('Lynn Smit', 'Smit');
+INSERT INTO contacts (name, contacts) VALUES ('Lisa Smits', 'Smits');
+INSERT INTO contacts (name, contacts) VALUES ('Saar Stichting', 'Stichting');
+INSERT INTO contacts (name, contacts) VALUES ('Fleur Veen', 'Veen');
+INSERT INTO contacts (name, contacts) VALUES ('Maud Ven', 'Ven');
+INSERT INTO contacts (name, contacts) VALUES ('Lieke Vermeulen', 'Vermeulen');
+INSERT INTO contacts (name, contacts) VALUES ('Sarah Visser', 'Visser');
+INSERT INTO contacts (name, contacts) VALUES ('Isa Vliet', 'Vliet');
+INSERT INTO contacts (name, contacts) VALUES ('Liv Vos', 'Vos');
+INSERT INTO contacts (name, contacts) VALUES ('Roos Vries', 'Vries');
+INSERT INTO contacts (name, contacts) VALUES ('Noa Wal', 'Wal');
+INSERT INTO contacts (name, contacts) VALUES ('Nora Willems', 'Willems');
+INSERT INTO contacts (name, contacts) VALUES ('Noor Wit', 'Wit');
+*/
